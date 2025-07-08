d5e776ab1338bceedce4f9bcccfa9ac725ef9c62
sekwan <sekwanoh1107@gmail.com>
2025-07-08 16:29:27 +0900

    .

--- src/components/ReleaseNote.vue
+++ src/components/ReleaseNote.vue
@@ -57,7 +57,7 @@ export default {
 					timestamp: "2025-04-04",
 				},
 				{
-					content: "신의상 스킬추가, 마신표 기본스탯 수정",
+					content: "신의상 스킬추가, 마신표 기본스탯 수정,누락흰딱추가",
 					timestamp: "2025-07-09",
 				},
 			],
